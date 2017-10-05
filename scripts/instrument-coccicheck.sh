81d87621a0d968ce35ba12861603c5b7fd01ceb6
Gianpaolo Macario <gianpaolo_macario@mentor.com>
2017-10-05 13:12:41 +0200

    instrument-coccicheck.sh: Save LOGFILE
    Signed-off-by: Gianpaolo Macario <gianpaolo_macario@mentor.com>

--- scripts/instrument-coccicheck.sh
+++ scripts/instrument-coccicheck.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
+set -e
 # set -x
 
-echo "INFO: $(date): Script start"
-echo "INFO: hostname=$(hostname)"
+SRC_DIR=~/linux-mainline
 
-cd ~/linux-mainline
-k=0
-find scripts/coccinelle -name "*.cocci" -print | while read smpl; do
+cd ${SRC_DIR}
+SRC_SHA=$(git rev-parse --verify HEAD)
+LOGFILE=coccicheck-${SRC_SHA}.log
+
+function body {
+  echo "INFO: $(date): Script start"
+  echo "INFO: Saving logfile to ${LOGFILE}"
+  echo "INFO: hostname=$(hostname)"
+  echo "INFO: SRC_DIR=${SRC_DIR}"
+  echo "INFO: SRC_SHA=${SRC_SHA}"
+  k=0
+  find scripts/coccinelle -name "*.cocci" -print | while read smpl; do
     echo "================================================================================================"
     k=$[ k + 1 ]
     echo "INFO: Applying SmPL ${k}: ${smpl}"
-    echo "INFO: make coccicheck COCCI=${smpl} MODE=report"
-    make coccicheck COCCI=${smpl} MODE=report V=1
-    echo "INFO: $(date): make coccicheck end"
-done
-echo "INFO: $(date): Script end"
+    CMDLINE="make coccicheck COCCI=${smpl} MODE=report V=1"
+    echo "INFO: Running ${CMDLINE}"
+    ${CMDLINE} || true
+    echo "INFO: $(date): Command completed"
+  done
+  echo "INFO: $(date): Script end"
+}
+
+body | tee ${LOGFILE}
 
 # EOF
