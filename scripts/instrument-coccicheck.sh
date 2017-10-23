051239276f68eb42f7c4276a75f2c41a9f276fdb
Gianpaolo Macario <gianpaolo_macario@mentor.com>
2017-10-23 16:39:36 +0100

    instrument-coccicheck.sh: Allow configurable COCCI_DIR and SRC_DIR
    Notice that spatch is now called instead than make coccicheck
    TODO: Should better understand how coccicheck script builds up spatch commandline
    
    Signed-off-by: Gianpaolo Macario <gianpaolo_macario@mentor.com>

--- scripts/instrument-coccicheck.sh
+++ scripts/instrument-coccicheck.sh
@@ -1,10 +1,17 @@
 #!/bin/bash
 
+# Configurable parameters - override them from commandline
+
+[ "${COCCI_DIR}" = "" ] && COCCI_DIR=~/linux-mainline/scripts/coccinelle
+[ "${SRC_DIR}" = "" ] && SRC_DIR=~/linux-mainline
+
+# ---------------------------------------------------------
+
 set -e
 # set -x
 
-SRC_DIR=~/linux-mainline
-
+cd ${COCCI_DIR}
+COCCI_SHA=$(git rev-parse --verify HEAD)
 cd ${SRC_DIR}
 SRC_SHA=$(git rev-parse --verify HEAD)
 LOGFILE=coccicheck-${SRC_SHA}.log
@@ -13,14 +20,18 @@ function body {
   echo "INFO: $(date): Script start"
   echo "INFO: Saving logfile to ${LOGFILE}"
   echo "INFO: hostname=$(hostname)"
+  echo "INFO: spatch_version=$(spatch --version | grep spatch)"
+  echo "INFO: COCCI_DIR=${COCCI_DIR}"
+  echo "INFO: COCCI_SHA=${COCCI_SHA}"
   echo "INFO: SRC_DIR=${SRC_DIR}"
   echo "INFO: SRC_SHA=${SRC_SHA}"
   k=0
-  find scripts/coccinelle -name "*.cocci" -print | while read smpl; do
+  find "${COCCI_DIR}" -name "*.cocci" -print | sort | while read smpl; do
     echo "================================================================================================"
     k=$[ k + 1 ]
     echo "INFO: Applying SmPL ${k}: ${smpl}"
-    CMDLINE="make coccicheck COCCI=${smpl} MODE=report V=1"
+    # CMDLINE="make coccicheck COCCI=${smpl} MODE=report V=1"
+    CMDLINE="spatch -D report --no-show-diff --very-quiet --cocci-file ${smpl} --dir ."
     echo "INFO: Running ${CMDLINE}"
     ${CMDLINE} || true
     echo "INFO: $(date): Command completed"
